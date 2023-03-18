6417cc40de176b1810968b5d4a079acccfbce6a6
temple <istanpeng@163.com>
2023-03-19 01:58:02 +0800

    生产者消费者模型

---
+++ include/ThreadTest/ConsumerModle.hpp
@@ -0,0 +1,63 @@
+//
+// Created by tanpe on 2023/3/19.
+//
+#pragma once
+
+#include <mutex>
+#include <thread>
+#include <future>
+#include <list>
+#include <queue>
+
+
+class ConsumerModel {
+public:
+    ConsumerModel(int n) {
+        for (int i = 0; i < n; ++i) {
+            threads_.push_back(std::thread([&](){
+
+                while (true) {
+                    std::unique_lock<std::mutex> lock(mux_);
+                    cv_.wait(lock, [&]()->bool {
+                        return !messages_.empty() || stop_;
+                    });
+                    if (stop_) {
+                        break;
+                    }
+
+                    if (!messages_.empty()) {
+                        auto message = messages_.front();
+                        messages_.pop();
+                        lock.unlock();
+                        message();
+                    }
+
+                }
+
+            }));
+        }
+    }
+    ~ConsumerModel() {
+        for (auto&& it: threads_) {
+            it.join();
+        }
+    }
+
+    void AddMessage(std::function<void()>&& func) {
+        std::unique_lock<std::mutex> lock(mux_);
+        messages_.push(func);
+        cv_.notify_all();
+    }
+    void Stop() {
+        std::unique_lock<std::mutex> lock(mux_);
+        stop_ = true;
+        cv_.notify_all();
+    }
+
+private:
+    std::list<std::thread> threads_;
+    std::mutex mux_;
+    std::condition_variable cv_;
+    bool stop_ = false;
+    std::queue<std::function<void()>> messages_;
+};
